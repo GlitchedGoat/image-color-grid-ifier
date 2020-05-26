95fb1e4e7ba24612ed2e1ad2a5c4684dfc8fd710
GlitchedGoat <58455756+GlitchedGoat@users.noreply.github.com>
2020-05-25 23:25:28 -0500

    set pixel in grid; add cli arg for grid spacing

--- ColorGridProg.cs
+++ ColorGridProg.cs
@@ -16,8 +16,11 @@ namespace image_color_grid_izer
             [Option('o', "outputfilepath", Required = false, HelpText = "Path of the resulting image to write")]
             public string OutputFilepath {get; set;}
 
-            [Option('s', "spacing", Required = false, HelpText = "grid size; how often to draw a full-color pixel")]
+            [Option('s', "spacing", Required = false, HelpText = "grid size; how often to draw a full-color pixell. default 5px")]
             public int? PixelSpacing {get; set;}
+
+            [Option('l', "linewidth", Required = false, HelpText = "line width, how wide each lin is. default 1px.")]
+            public int? LineWidth {get; set;}
         }
 
         public static Image<Rgba32> loadImage(string filepath) {
@@ -27,9 +30,8 @@ namespace image_color_grid_izer
             return loaded.CloneAs<Rgba32>();
         }
 
-        public static Image<Rgba32> convertImage(Image<Rgba32> input, int spacing) {
+        public static Image<Rgba32> convertImage(Image<Rgba32> input, int spacing, int linewidth) {
             System.Console.WriteLine($"Converting image of dimensions: {input.Width} by {input.Height}");
-            // var converted = new Image<Rgba32>(input.Width, input.Height);
             // Note: GrayScale() can accept an arg
             // https://docs.sixlabors.com/api/ImageSharp/SixLabors.ImageSharp.Processing.GrayscaleExtensions.html
             var converted = input.Clone(s => s.Grayscale());
@@ -37,7 +39,14 @@ namespace image_color_grid_izer
             // each pixel at once
             for (int x = 0; x < input.Width; x++) {
                 for (int y = 0; y < input.Height; y++) {
-                    if (x % spacing == 0 || y % spacing == 0) {
+                    var xSpacing = x % spacing;
+                    var ySpacing = y % spacing;
+
+                    // so if it is 0, that means match... otherwise allow + some number.
+                    // can't be greater than the spacing... number of spacing is 0..spacing
+                    // So if x/ySpacing is 0..lw
+                    // if x/ySpacing < linewidth, allow it
+                    if (xSpacing < linewidth || ySpacing < linewidth) {
                         // use original pixel color
                         converted[x, y] = input[x, y];
                     }
@@ -54,11 +63,16 @@ namespace image_color_grid_izer
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed<Options>(o => {
+                    var pixelSpacing = o.PixelSpacing ?? 10;
+                    var lineWidth = o.LineWidth ?? 1;
+
+                    if (lineWidth >= pixelSpacing) {
+                        System.Console.WriteLine($"pixel spacing of {pixelSpacing} cannot be >= line width of {lineWidth}!");
+                    }
                     var img = loadImage(o.Filepath);
-                    var converted = convertImage(img, o.PixelSpacing ?? 10);
+                    var converted = convertImage(img, o.PixelSpacing ?? 10, o.LineWidth ?? 1);
                     saveImage(converted, o.OutputFilepath ?? $"{o.Filepath}-converted.png");
                 });
         }
